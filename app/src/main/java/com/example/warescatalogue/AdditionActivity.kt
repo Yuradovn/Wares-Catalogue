b563ecb232d52348dc97da32db6f1d458d1375a1
YuraKrulik <yurii.krulik@gmail.com>
2023-12-21 19:03:30 +0200

    Inplemented create logic

--- app/src/main/java/com/example/warescatalogue/AdditionActivity.kt
+++ app/src/main/java/com/example/warescatalogue/AdditionActivity.kt
@@ -2,10 +2,34 @@ package com.example.warescatalogue
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.warescatalogue.database.AppDatabase
+import com.example.warescatalogue.databinding.ActivityAdditionBinding
+import com.example.warescatalogue.entity.Ware
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 class AdditionActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAdditionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_addition)
+        binding = ActivityAdditionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonAdd.setOnClickListener { addWare() }
+    }
+
+    private fun addWare() {
+        val name = binding.name.text.toString()
+        val description = binding.description.text.toString()
+        val price = (binding.price.text.toString().toDouble() * 100).roundToInt()
+
+        lifecycleScope.launch {
+            val ware = Ware(name = name, description = description, price = price)
+            AppDatabase(this@AdditionActivity).getWareDao().addWare(ware)
+
+            finish()
+        }
     }
 }
